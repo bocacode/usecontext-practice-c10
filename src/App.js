6f76124da2f09fd0c0994314fc98c70e3f4477c6
jSohnData <jihos@me.com>
2023-04-24 19:59:46 -0400

    UPDATED cleaned code

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import Page from './components/Page.jsx';
 import './App.css';
 
 // 2) Create and export a context object
-
-/* Why is the context object capitlized?
-It's a convention to capitalize the first letter of constants that hold context objects, so that it's clear they are special objects that have a significant role in the application's architecture. This helps make it easier for other developers to understand the code and to work with the context object in their own components. */
 export const UserStatus = createContext(null);
 
 function App() {
@@ -16,9 +13,9 @@ function App() {
     <>
       {/* 3) Establish a provider */}
       <UserStatus.Provider value={[signedIn, setSignedIn]}>
-        <h1>{signedIn ? 
-          "Click below to Sign Out" : 
-          "Click below to Please Sign In"}</h1>
+        <h1>{signedIn
+          ? "Take Care. Click below to Sign Out"
+          : "Welcome! Click below to Please Sign In"}</h1>
         <Page />
       </UserStatus.Provider>      
     </>
