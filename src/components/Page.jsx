740cb1881c4802ae4b255659188177f47bc3e8b7
Jiho Sohn <jiho@Boca-Code-Presentations.local>
2023-04-24 17:30:02 -0400

    FIRST commit

---
+++ src/components/Page.jsx
@@ -0,0 +1,14 @@
+import SubPage from "./SubPage.jsx";
+import Button from "./Button.jsx";
+
+
+export default function Page () {
+    return (
+        <>
+            <h2>Page</h2>
+            <Button />
+            <SubPage />            
+        </>
+    )
+}
+
---
+++ src/components/SubPage.jsx
@@ -0,0 +1,11 @@
+import Button from "./Button.jsx";
+
+export default function SubPage () {
+
+    return (
+        <>
+            <h2>SubPage</h2>
+            <Button />
+        </>
+    )
+}
